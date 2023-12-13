1dfea4d7b714f54500cf384571c8bd2974711840
Byron Hambly <byron@hambly.dev>
2023-12-13 14:39:26 +0200

    23.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "nixos config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # hardware.url = "github:nixos/nixos-hardware";
